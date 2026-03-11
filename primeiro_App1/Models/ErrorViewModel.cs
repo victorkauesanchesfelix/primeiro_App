9d9e7e8f6003d11927a596880f4f10e59b7e4d2d
victorkauesanchesfelix <victorksfelix2008@gmail.com>
2026-03-11 17:18:43 -0300

    Adicionando projeto

---
+++ primeiro_App1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace primeiro_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
