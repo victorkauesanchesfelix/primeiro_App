d2ce64eabc937b5dba1a2f76e32cc4aedb8b5f0f
victorkauesanchesfelix <victorksfelix2008@gmail.com>
2026-03-18 17:58:28 -0300

    update Action teste, resultado

--- primeiro_App/Controllers/AlunoController.cs
+++ primeiro_App/Controllers/AlunoController.cs
@@ -10,5 +10,18 @@ namespace primeiro_App.Controllers
             ViewData["Escola"] = "E.E. Anhanguera";
             return View();
         }
+        public IActionResult Teste()
+        {
+            TempData["MSG_EX"] = "Mensagem do TempData";
+            return RedirectToAction("Resultado");
+        }
+
+        public IActionResult Resultado()
+        {
+            if (TempData["MSG_EX"] != null)
+                ViewBag.Exemplo = "Aluno ETEC MB";
+            return View();
+        }
+
     }
 }
